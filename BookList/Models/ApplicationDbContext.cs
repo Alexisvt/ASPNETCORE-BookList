9e11010fe1cb43fc038f5c6d7a965780240c1918
Bob <bob@example.com>
2018-01-16 10:47:17 -0600

    initial commit

---
+++ BookList/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookList.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
---
+++ BookList/Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace BookList.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
